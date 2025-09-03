49a58adde32ac7f8138a1659e4640dc9967b8037
atharvdsingh <dsingh9450492600@gmail.com>
2025-09-03 22:41:17 +0530

    done with user creation route

--- week13-2/my-app/app/api/user/route.ts
+++ week13-2/my-app/app/api/user/route.ts
@@ -1,13 +1,59 @@
-import { log } from "console"
-import { NextRequest, NextResponse } from "next/server"
-export async function GET(){
-        return NextResponse.json({
-            user:"dsingh9450492600@gmail.com",
-            name:"atharv dev singh"
-        })
+import { PrismaClient } from "@prisma/client";
+import { log } from "console";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function GET() {
+  return NextResponse.json({
+    user: "dsingh9450492600@gmail.com",
+    name: "atharv dev singh",
+  });
 }
-export async function POST(req:NextRequest) {
-    const body=await req.json()
-    return NextResponse.json(body)
-    
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const existedUser = await prisma.users.findFirst({
+      where: {
+        email: body.email,
+      },
+    });
+    if (existedUser) {
+      return NextResponse.json(
+        {
+          message: "user already exist",
+        },
+        {
+          status: 409,
+        }
+      );
+    }
+
+    const user = await prisma.users.create({
+      data: {
+        email: body.email,
+        password: body.password,
+
+      },
+    });
+    if (!user) {
+      return NextResponse.json(
+        {
+          message: "can't create the user-something went wrong",
+        },
+        {
+          status: 500,
+        }
+      );
+    }
+    return NextResponse.json(user, {
+      status: 200,
+    });
+  } catch (error:any) {
+    return NextResponse.json({
+        message:error?.message || "some thing went wrong"
+    },{
+        status:500
+    })
+  }
 }
